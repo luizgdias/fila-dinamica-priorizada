96164035187dd1f7b5afaf72a23976900d41794f
lgdias <gusttavodiias@gmail.com>
2018-06-23 23:41:51 -0300

    comentarios na classe principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,34 @@
+/*
+ * Algoritmo que simula uma fila priorizada. São criadas listas clientes, de assuntos padrão e personalizada para cada cliente.
+ * 	-> É criada uma fila voltada para recepcionar clientes que recebe o cliente, sua lista de assunto e a hora da sua chegada.
+ * 		'-> Isso é realizado através da função recepcionar();
+ * 	-> Após enfileirar os clientes e seus assuntos, eles são atendidos, neste momento a fila é convertida em um heap que atende o
+ * 		cliente com maior prioridade.
+ * 		'-> Isso é realizado através da função atender();
+ * 	-> Após atender atender o cliente e suas demandas, o mesmo é removido da fila.
+ * 		'-> Isso é realizado através da função encerrar();
+ * 	-> Após encerrar, informações estatísticas são incrementadas e podem ser consultadas.
+ * 		'-> Isso é realizado através da função gerarEstatística;
+ * 
+ * Código desenvolvido para a disciplina de Estrutura de Dados do Programa de Pós-Graduação UFF/IC - Turma 2018-1
+ * Aluno: Luiz Gustavo Dias
+ * email: gusttavodiias@gmail.com
+ * link github: https://github.com/luizgdias/fila-dinamica-priorizada
+ */
+
 public class Principal {
 
 	public static void main(String[] args) {
 		/*************************************************************************************
-		 * Criando tipo de assuntos, assuntos, e lista de assuntos por cliente
+		 * Criando tipo de assunto
 		 *************************************************************************************/
 		TipoAssunto pf = new TipoAssunto("pf", "pessoa física", 20);
 		TipoAssunto pj = new TipoAssunto("pj", "pessoa jurídica", 10);
 		TipoAssunto nc = new TipoAssunto("nc", "não correntista", 01);
 		
+		/*************************************************************************************
+		 * Criando assuntos
+		 *************************************************************************************/
 		Assunto saquePf = new Assunto();
 		saquePf.setTipoAssunto(pf);
 		saquePf.setDescricao("saque pf ");
@@ -29,16 +50,24 @@ public class Principal {
 		pagamento.setProvidencia("pagar");
 		pagamento.setProximoAssunto(null);
 		
+		/*************************************************************************************
+		 * Criando lista de assuntos padrão
+		 *************************************************************************************/
 		ListaAssuntos listaPadrao = new ListaAssuntos();
 		listaPadrao.inserirNoFinal(saquePf);
 		listaPadrao.inserirNoFinal(depositoPj);
 		listaPadrao.inserirNoFinal(pagamento);
 		listaPadrao.inserirNoFinal(depositoPj);
 		
+		/*************************************************************************************
+		 * Criando clientes e suas respectivas listas de assunto
+		 *************************************************************************************/
+		Cliente Luiz 		= new Cliente(1010, "Luiz	");
 		ListaAssuntos assuntosLuiz = new ListaAssuntos();
 		assuntosLuiz.inserirNoFinal(depositoPj);
 		assuntosLuiz.inserirNoFinal(saquePf);
 		
+		Cliente LuizC 		= new Cliente(1011, "Luic C	");
 		ListaAssuntos assuntosLuizC = new ListaAssuntos();
 		assuntosLuizC.inserirNoFinal(pagamento);
 		assuntosLuizC.inserirNoFinal(pagamento);
@@ -46,23 +75,17 @@ public class Principal {
 		assuntosLuizC.inserirNoFinal(pagamento);
 		assuntosLuizC.inserirNoFinal(depositoPj);
 		
+		Cliente Kelli 		= new Cliente(1012, "Kelli	");
 		ListaAssuntos assuntosKelli = new ListaAssuntos();
 		assuntosKelli.inserirNoFinal(saquePf);
 		
+		Cliente Carlos 		= new Cliente(1012, "Carlos	");
 		ListaAssuntos assuntosCarlos = new ListaAssuntos();
 		assuntosCarlos.inserirNoFinal(saquePf);
 		assuntosCarlos.inserirNoFinal(depositoPj);
-
-		/*************************************************************************************
-		 * Criando cliente
-		 *************************************************************************************/
-		Cliente Luiz 		= new Cliente(1010, "Luiz	");
-		Cliente LuizC 		= new Cliente(1011, "Luic C	");
-		Cliente Kelli 		= new Cliente(1012, "Kelli	");
-		Cliente Carlos 		= new Cliente(1012, "Carlos	");
 		
 		/*************************************************************************************
-		 * Enfileirando os atendimentos
+		 * Enfileirando os atendimentos - função recepcionar()
 		 *************************************************************************************/
 		ListaAtendimentos listaDeAtendimentos = new ListaAtendimentos();
 		listaDeAtendimentos.recepcionar(Luiz	, assuntosLuiz	, 10);
@@ -72,9 +95,8 @@ public class Principal {
 		listaDeAtendimentos.recepcionar(Kelli	, assuntosKelli	, 12);
 
 
-		
 		/*************************************************************************************
-		 * Realizar atendimento de cliente
+		 * Realizar atendimento de cliente - para atender clientes com maior prioridade - função atender()
 		 *************************************************************************************/
 		FilaAtendimentoPrioridade listaDeAtendimentosP = new FilaAtendimentoPrioridade(listaDeAtendimentos.qtdAtendimentos);
 		
